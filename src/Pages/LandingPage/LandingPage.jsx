fab74f02b24a74e4f18ede14e1fc47029d94067f
Jason Ngov <40685033+jasonngov@users.noreply.github.com>
2023-01-18 22:12:01 -0500

    Added landing page and integrated in React Router

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Header from '../../Components/Header/Header'
+import './LandingPage.css'
 
 export default function LandingPage() {
   return (
     <div>
       <Header />
       <Navbar />
+      <div className='landingPageContent'>
+        <h1>Verifiable Legal Entity Identifier (vLEI)</h1>
+      </div>
     </div>
   )
 }
