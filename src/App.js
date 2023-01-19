764e3e7cfbdea5cf945a2e155e0979c86c5b6eaf
Jason Ngov <40685033+jasonngov@users.noreply.github.com>
2023-01-18 22:12:01 -0500

    Added landing page and integrated in React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Login from './Pages/Login/Login';
 import './App.css'
+import Login from './Pages/Login/Login';
+import LandingPage from './Pages/LandingPage/LandingPage';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  /*
+    * TODO:
+    * Add routes for Create and View components
+  */
   return (
     <div className='background'>
-      <Login/>
+      <Routes>
+        <Route path="/" element={<Login />} />       
+        <Route path="/home" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Header from '../../Components/Header/Header'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  )
+}
